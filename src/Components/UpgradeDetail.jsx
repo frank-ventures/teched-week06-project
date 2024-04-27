d3a5c47cc8b74e9612ef361b668abdfa19aa5941
frank-ventures <6458936+frank-ventures@users.noreply.github.com>
2024-04-27 17:11:48 +0100

    local storage working. Reset game working. Very stable version.

--- src/Components/UpgradeDetail.jsx
+++ src/Components/UpgradeDetail.jsx
@@ -3,17 +3,30 @@ export default function UpgradeDetail({
   content,
   className,
   suffix
-  //   upgradeName
 }) {
-  //   const suffix = upgradeName === "extraAinsleys" ? "" : " Ħps";
-
-  return (
-    <div className="individual-detail-row flex">
-      <p className={className}>{description}</p>{" "}
-      <p className="upgrade-content">
-        {content}
-        {suffix}
-      </p>
-    </div>
-  );
+  // This spicy little number was a bit of help from GPT, who informed me about "Array.isArray". The prompt:
+  // With other 'upgrades', this component displays the number which is stored in the userStats object. However, for extraAinsleys, it's now showing the text stored in the array, so the display on the page is now a long list of image sources.
+  // How would I fix this? Would I use a conditional inside the component which says, 'if the parameter contains "assets" then display upgradename.length?
+  // And sure enough, if the 'content' parameter 'isArray', then I can return content.length instead of an endless string of image locations. Beautiful. Nice and hot.
+  if (Array.isArray(content)) {
+    return (
+      <div className="individual-detail-row flex">
+        <p className={className}>{description}</p>{" "}
+        <p className="upgrade-content">
+          {content.length}
+          {suffix}
+        </p>
+      </div>
+    );
+  } else {
+    return (
+      <div className="individual-detail-row flex">
+        <p className={className}>{description}</p>{" "}
+        <p className="upgrade-content">
+          {content}
+          {suffix}
+        </p>
+      </div>
+    );
+  }
 }
